6b3eed9c30ac7f43279ed867598c77a2c29857aa
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-06 14:27:20 -0600

    Add location poro

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -2,7 +2,11 @@ class Location
   attr_accessor :city, :state, :lat, :long
 
   def initialize(location_json)
-    
-    binding.pry
+    @city = location_json[:results][0][:address_components][0][:long_name]
+    @state = location_json[:results][0][:address_components][2][:short_name]
+    @county = location_json[:results][0][:address_components][1][:long_name]
+    @country = location_json[:results][0][:address_components][3][:long_name]
+    @lat = location_json[:results][0][:geometry][:location][:lat]
+    @long = location_json[:results][0][:geometry][:location][:long]
   end
 end
