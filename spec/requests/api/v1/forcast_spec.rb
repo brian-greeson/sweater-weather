343d12f334c4634ce4578529b3e744b685e62f4b
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-07 09:25:08 -0600

    Update forecast spec with forecast expectations

--- spec/requests/api/v1/forcast_spec.rb
+++ spec/requests/api/v1/forcast_spec.rb
@@ -19,8 +19,30 @@ RSpec.describe "forcast api", type: :request  do
       expect(response_json[:included][:location][:long]).to eq(-104.990251)
     end
 
-    xit "provides forcast data at the provided location" do
+    it "provides forcast data at the provided location" do
+      VCR.use_cassette('forecast') do
+        location_params = {location: "denver, co"}
+        get @api_url + "forecast", params: location_params
+      end
+      response_json = JSON.parse(response.body, symbolize_names: true)
 
+      expect(response_json[:data][:forecast][:current][:date]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:current][:feels_like]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:current][:humidity]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:current][:Visability]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:current][:uv_index]).to eq("10/10/99")
+
+
+      expect(response_json[:data][:forecast][:hourly][:date]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:hourly][:summary]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:hourly][:summary_img]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:hourly][:temp]).to eq("10/10/99")
+
+      expect(response_json[:data][:forecast][:day][:date]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:day][:accumilation]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:day][:temp_high]).to eq("10/10/99")
+      expect(response_json[:data][:forecast][:day][:temp_low]).to eq("10/10/99")
+  
     end
   end
 end
