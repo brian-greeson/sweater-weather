d0f68fa5f2826949bbb958db16133ce6fac17cb0
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-08 19:53:10 -0600

    Update test to expect strings

--- spec/requests/api/v1/user_spec.rb
+++ spec/requests/api/v1/user_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "when a client makes a request to /user" do
+  after(:all) do
+    User.destroy_all
+  end
   it "With the correct params they can create a user in the db" do
     user_params =
     {
@@ -10,14 +13,13 @@ RSpec.describe "when a client makes a request to /user" do
     }
     get '/api/v1/users', params: user_params
     response_json = JSON.parse(response.body, symbolize_names: true)
-
     user = User.last
 
     expect(response.status).to eq(201)
-    expect(response_json[:data][:type]).to eq("users")
-    expect(response_json[:data][:id]).to eq(user.id)
-    expect(response_json[:data][:email]).to eq(user.email)
-    expect(response_json[:data][:api_key]).to_not eq(user.api_key)
+    expect(response_json[:data][:type]).to eq("user")
+    expect(response_json[:data][:id]).to eq(user.id.to_s)
+    expect(response_json[:data][:attributes][:email]).to eq(user.email)
+    expect(response_json[:data][:attributes][:api_key]).to_not eq(nil)
 
   end
 
