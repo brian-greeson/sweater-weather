e5f7f58aa680a2c3576a4c0463a767dc3afa7404
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 14:44:28 -0600

    Add validation testing to trip model

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it {should validate_presence_of(:start)}
+    it {should validate_presence_of(:end)}
+    it {should validate_presence_of(:travel_time)}
+    it {should validate_presence_of(:arrival_forecast)}
+  end
 end
