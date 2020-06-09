6fc822e8474b71ae34e45200185c928ee792d846
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 14:38:11 -0600

    Login sad path test

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "when client makes a post request to /sessions" do
 
   describe "With invalid credentials" do
     it "response with status 400 and error message" do
-      user = User.create!(email: "whatever@example.com", password: "password", password_confirmation: "password")
+      User.create!(email: "whatever@example.com", password: "password", password_confirmation: "password")
       login_params =
       {
         "email": "whatever@example.com",
