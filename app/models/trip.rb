cd6ccc7c42c9220527fdd92a77995c3862684d3a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 14:44:38 -0600

    Add trip validations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,8 @@
 class Trip < ApplicationRecord
+  validates_presence_of :start
+  validates_presence_of :end
+  validates_presence_of :travel_time
+  validates_presence_of :arrival_forecast
+
+
 end
