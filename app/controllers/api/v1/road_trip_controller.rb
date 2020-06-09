d1ca5971bd3eb872ab175df35478969c98423f8c
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 15:44:18 -0600

    WIP happy path road_trip

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,14 +2,19 @@ class Api::V1::RoadTripController < Api::V1::BaseController
   before_action :current_user
 
   def create
-
    trip = current_user.trips.create(trip_params)
-   if trip.is_valid?
+   if trip.valid?
      response.status = :ok
      render json: TripSerializer.new(trip).serialized_json
    else
      response.status = :bad_request
      render json: {errors: trip.errors.full_messages.to_sentence}
    end
+  end
+
+  private
+
+  def trip_params
+    params.permit(:origin, :destination)
   end
 end
