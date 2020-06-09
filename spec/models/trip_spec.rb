bf43d30b55a2b2a53cf4604de8f00bb9ed1e125f
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 15:43:40 -0600

    Add relationship test to models

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
   describe "validations" do
-    it {should validate_presence_of(:start)}
-    it {should validate_presence_of(:end)}
+    it {should validate_presence_of(:origin)}
+    it {should validate_presence_of(:destination)}
     it {should validate_presence_of(:travel_time)}
     it {should validate_presence_of(:arrival_forecast)}
+  end
+
+  describe "releationships" do
+    it {should belong_to(:user)}
   end
 end
