50e11b371f6db4cdaacf61acd7f97d48dd0c29c2
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 14:41:38 -0600

    Added trips table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_234913) do
+ActiveRecord::Schema.define(version: 2020_06_09_204112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.string "start"
+    t.string "end"
+    t.string "travel_time"
+    t.string "arrival_forecast"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
