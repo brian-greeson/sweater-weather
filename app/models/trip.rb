8f29cfd016cc6a60a3e4ebffa3ee61111b28afd3
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 15:43:52 -0600

    Add relationships to models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,18 @@
 class Trip < ApplicationRecord
-  validates_presence_of :start
-  validates_presence_of :end
+  before_validation :determine_travel, :determine_forcast
+  validates_presence_of :origin
+  validates_presence_of :destination
   validates_presence_of :travel_time
   validates_presence_of :arrival_forecast
 
+  belongs_to :user
+
+
+  def determine_travel
+    
+  end
+
+  def determine_forcast
+  end
 
 end
