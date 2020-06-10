25775fdb22582bafd96abe0acebdab0d426a96b3
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 18:46:26 -0600

    Remove presence validation of trip attributes travel time and forecast

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -4,11 +4,9 @@ RSpec.describe Trip, type: :model do
   describe "validations" do
     it {should validate_presence_of(:origin)}
     it {should validate_presence_of(:destination)}
-    it {should validate_presence_of(:travel_time)}
-    it {should validate_presence_of(:arrival_forecast)}
   end
 
-  describe "releationships" do
+  describe "releationships", :vcr do
     it {should belong_to(:user)}
   end
 end
