30b8b348c81f74f2f95971dd3faffc1a7cafeb4e
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 18:49:32 -0600

    Added trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,18 +1,42 @@
 class Trip < ApplicationRecord
-  before_validation :determine_travel, :determine_forcast
+  before_create :attributes_from_directions
   validates_presence_of :origin
   validates_presence_of :destination
-  validates_presence_of :travel_time
-  validates_presence_of :arrival_forecast
 
   belongs_to :user
 
 
-  def determine_travel
-    
+  def attributes_from_directions
+    directions = Directions.new(DirectionsService.road_trip(self))
+    self.travel_time = directions.travel_time
+    self.origin = directions.origin
+    self.destination = directions.destination
+    self.arrival_forecast = forecast_at_time_of_arrival(directions).description
   end
 
-  def determine_forcast
+
+  private
+
+  def arrival_time(directions)
+    Time.at(directions.travel_time_seconds.seconds.from_now)
+  end
+
+  def date_to_hour(time)
+    time.strftime("%l %p").strip
+  end
+
+  def destination_forecast(directions)
+    ForecastService.forecast(Location.new(
+      {
+        lat: directions.destination_location[:lat],
+        long: directions.destination_location[:lng]
+      }))
+  end
+
+  def forecast_at_time_of_arrival(directions)
+    destination_forecast(directions).hourly.select do |forecast|
+      forecast.time == date_to_hour(arrival_time(directions))
+    end.first
   end
 
 end
