228b37dee8408c361949a5b24ae037bf5ff3c23f
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-09 18:49:04 -0600

    Update location with class method for inializing with service json vs hash

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -1,13 +1,28 @@
 class Location
   attr_accessor :city, :state, :lat, :long, :place_id
 
-  def initialize(location_json)
-    @city = location_json[:results][0][:address_components][0][:long_name]
-    @state = location_json[:results][0][:address_components][2][:short_name]
-    @county = location_json[:results][0][:address_components][1][:long_name]
-    @country = location_json[:results][0][:address_components][3][:long_name]
-    @lat = location_json[:results][0][:geometry][:location][:lat]
-    @long = location_json[:results][0][:geometry][:location][:lng]
-    @place_id = location_json[:results][0][:place_id]
+  def initialize(location_params = {})
+    @city = location_params[:city]
+    @state = location_params[:state]
+    @county = location_params[:county]
+    @country = location_params[:country]
+    @lat = location_params[:lat]
+    @long = location_params[:long]
+    @place_id = location_params[:place_id]
   end
+
+  def self.from_location_service(json)
+    location_params =
+    {
+      city: json[:results][0][:address_components][0][:long_name],
+      state: json[:results][0][:address_components][2][:short_name],
+      county: json[:results][0][:address_components][1][:long_name],
+      country: json[:results][0][:address_components][3][:long_name],
+      lat: json[:results][0][:geometry][:location][:lat],
+      long: json[:results][0][:geometry][:location][:lng],
+      place_id: json[:results][0][:place_id]
+    }
+    Location.new(location_params)
+  end
+
 end
